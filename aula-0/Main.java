1e70e5500670b81cc6dcc42d309a9ec23be2496d
Felipe Gabriel <Felipegf0710@gmail.com>
2025-04-26 11:31:34 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,40 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+	    
+	   Scanner entrada = new Scanner (System.in);
+	    
+	    System.out.print ("Digite o valor da laranja : ");
+	    int laranja = entrada.nextInt();
+	    
+	    System.out.print("Digite o valor da uva : ");
+	    int uva = entrada.nextInt();
+	    
+	    
+	    System.out.println("Resultado da soma = " + (laranja + uva));
+	    
+	  
+	     
+	    char primeiro_caractere = 'f'; 
+	    int idade = 15;
+	    boolean cafe_da_manha = true;
+	    
+	    double media_olimpiadas = 9.90; /**teste*/
+	    float medis_provas = 5.50f; /**teste*/
+	    
+	    int uva_e_laranja=laranja+uva;
+	  
+	    
+	  
 		System.out.println("Hello World");
+		System.out.println ("Receba");
+
+	
+		
+	 
+		
 	}
 }
+
+
