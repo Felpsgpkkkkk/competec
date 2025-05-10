8ab610c24c270ee9f9b467ba0130059f4bbce41a
Felipe Gabriel <Felipegf0710@gmail.com>
2025-05-10 11:50:11 -0300

    Update Main.java

--- aula-1/Main.java
+++ aula-1/Main.java
@@ -1,7 +1,71 @@
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+        
+public class Main
+{
+	public static void main(String[] args) {
+	
+	
+		System.out.println("Hello World");
+	
+	boolean Gleba = true;
+	boolean Matheus = true;
+	boolean EhIgual = Gleba != Matheus;
+    System.out.println("Gleba" == "Matheus");
+
+    int aluno_idade = 15;
+    int professor_idade = 25;
+    boolean EhMenor = aluno_idade < professor_idade;
+    System.out.println("tudo veridico, o aluno é mais novo que o professor!");
+    
+    int p = 5;
+   
+   
+    if ( (p > 6))
+    {
+        System.out.println ("executa");
+    }
+    
+    
+    else if (p==5) 
+    {
+        System.out.println ("n executa");
+    }
+    boolean chovendo = true;
+    boolean relampejando = true;
+    
+    
+    if (relampejando&&chovendo)
+    {
+        System.out.println ("não sairei");
+    }
+    else if (chovendo)
+    {
+        System.out.println ("sairei");
+    }
+    else {
+        System.out.println ("sairei");
+    }
+    
+    
+    
+       boolean macarrao = true;
+       boolean estrogonofe = true;
+        
+    if (macarrao||estrogonofe)
+    {
+        System.out.println ("fico muito feliz");
+    }
+     else 
+     {
+        System.out.println ("fico triste");
+     }
+       
+    }
+    
+	}
+	
 		System.out.println("Hello World");
 	}
 }
