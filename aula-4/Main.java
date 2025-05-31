47147882bc7cdc20474648c18a7436858713d166
Felipe Gabriel <Felipegf0710@gmail.com>
2025-05-31 11:15:31 -0300

    Update Main.java

--- aula-4/Main.java
+++ aula-4/Main.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
-	}
-}
+	Scanner entrada = new Scanner (System.in);	
+	System.out.println("insira o tamanho da array: ");
+    int num = entrada.nextInt();
+    int arranjo[] = new int [num];
+    System.out.printf("quais os numeros serão utilizados?");
+    for(int i = 0; i < arranjo.length; i++){
+        arranjo[i] = entrada.nextInt( );
+        
+    }
+    System.out.printf("a array é: ");
+    for (int i = 0; i < arranjo.length; i++){
+        System.out.print (arranjo[i] + " ");
+       
+    }
+	}        
+  	}
+	
+
+
+
+
+
+	//  int num = entrada.nextInt();
+       /* for (int i = 1; i <= 100; i++){
+            System.out.print(10 * i);
+            System.out.print(" "); 
+            int numeros[] = {2, 6, 5};
+            int soma;
+            soma = numeros[0] + numeros[1] + numeros[2];
+            numeros[0] = 2;
+            numeros[1] = 6;
+            numeros[2] = 5;
+            System.out.print (soma); */
+            
