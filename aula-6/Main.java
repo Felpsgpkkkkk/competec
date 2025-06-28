f1f4a7c714d395a4b7a31f34ed5de0a44a3d64a7
Felipe Gabriel <Felipegf0710@gmail.com>
2025-06-28 11:29:39 -0300

    Update Main.java

--- aula-6/Main.java
+++ aula-6/Main.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		Scanner entrada = new Scanner (System.in);	
+	/*	System.out.printf("Digite seu nome : ");
+		String nome = entrada.nextLine();
+		String stringSemEspacos = nome.replaceAll("\\s+", "");
+		int tamanho = nome.length();
+		System.out.print("Olá " + nome + ", Seu nome tem " + tamanho + " caracteres");
+		
+		
+		System.out.printf("Digite a palavra secreta : ");
+		String senha = "BATATA";
+		String palavra = entrada.nextLine();
+		boolean resultado = (palavra.equalsIgnoreCase(senha));
+        System.out.println(senha.equals("BATATA"));
+        if (resultado == true)
+        {
+            System.out.printf("Parabéns, voce acertou!");
+        }
+        else {
+            System.out.printf("Que pena, voce errou!");
+        }
+		*/
+		
+    
+
+        
+       
+		
+		
+		
 	}
 }
