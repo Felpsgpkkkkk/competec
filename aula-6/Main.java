d0a65f8cbdaf48b2adb00600ee539fd6668a9ca9
Felipe Gabriel <Felipegf0710@gmail.com>
2025-08-02 11:38:55 -0300

    Update Main.java

--- aula-6/Main.java
+++ aula-6/Main.java
@@ -33,3 +33,40 @@ public class Main
 		
 	}
 }
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+		
+		String nome_produto1 = "morango do amor";
+	double preco1 = 20.99;
+	int quantidade1 = 6;
+	String nome_produto2 = "labubu";
+	double preco2 = 9.99;
+	int quantidade2 = 3;
+	String nome_produto3 = "pistache";
+	double preco3 = 11.50;
+	int quantidade3 = 10;
+	double estoque1 = preco1 * quantidade1;
+    	System.out.println (nome_produto1 + " " + estoque1 + " R$");
+	double estoque2 = preco2 * quantidade2;
+	    System.out.println (nome_produto2 + " " +estoque2 + " R$");
+	double estoque3 = preco3 * quantidade3;
+	    System.out.println (nome_produto3 + " " + estoque3 + " R$");
+	
+    double maior;
+    
+	    if (estoque1 > estoque2) {
+            maior = estoque1;
+        } else {
+            maior = estoque2;
+        }
+       
+        if (estoque3 > maior) {
+            maior = estoque3;
+        }
+        System.out.println(" ");
+        System.out.println("O maior valor entre os produtos em estoque é: " + maior + " R$");
+        }
+	}
